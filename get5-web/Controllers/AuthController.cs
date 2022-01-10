5a528fcb54d77555b29dd36ad5523db147c44afa
Brandon Stoeckel <Brandonrs522@gmail.com>
2022-01-10 10:59:13 -0500

    Added WIP auth controller Angular proxy redirects api Removed default backend

---
+++ get5-web/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using AspNetCore.Authentication.CAS;
+using AspNetCore.Identity.Mongo.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace get5_web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private SignInManager<MongoUser> _signInManager;
+
+        public AuthController(SignInManager<MongoUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet("umd")]
+        public IActionResult AuthUMD()
+        {
+            var authProperties = _signInManager
+                .ConfigureExternalAuthenticationProperties(CasDefaults.AuthenticationScheme.SCHEME,
+                "/api/auth/umd");
+
+            return Challenge(authProperties, CasDefaults.AuthenticationScheme.SCHEME);
+        }
+    }
+}
--- get5-web/Program.cs
+++ get5-web/Program.cs
@@ -1,6 +1,40 @@
+using AspNetCore.Authentication.CAS;
+using AspNetCore.Identity.Mongo;
+using AspNetCore.Identity.Mongo.Model;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddIdentityMongoDbProvider<MongoUser>(mongo =>
+{
+    mongo.ConnectionString = "mongodb://127.0.0.1:27017";
+});
+
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = CasDefaults.AuthenticationScheme.SCHEME;
+        options.DefaultChallengeScheme = CasDefaults.AuthenticationScheme.SCHEME;
+        options.DefaultSignInScheme = CasDefaults.AuthenticationScheme.SCHEME;
+    })
+    .AddCookie()
+    .AddCas(casOptions =>
+    {
+        casOptions.CasServerUrlBase = "https://shib.idm.umd.edu/shibboleth-idp/profile/cas";
+        casOptions.Renew = false;
+        // casOptions.SaveTokens = true;
+        casOptions.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+        casOptions.CorrelationCookie = new CookieBuilder
+        {
+            HttpOnly = false,
+            SameSite = SameSiteMode.Strict,
+            SecurePolicy = CookieSecurePolicy.SameAsRequest,
+            Expiration = TimeSpan.FromMinutes(10)
+        };
+    });
 
 builder.Services.AddControllersWithViews();
 
@@ -13,6 +47,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
