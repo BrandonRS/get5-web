34766daf3db9bec9e7ba0f14a32e41efc984ea63
Andylovesmangos <78506813+Andylovesmangos@users.noreply.github.com>
2022-02-15 08:55:14 -0500

    Created models folder and changed pathing in authservice. Perfectly Reverted HomeComponent.

--- get5-web/ClientApp/src/app/services/auth.services.ts
+++ get5-web/ClientApp/src/app/services/auth.services.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { UserInfo } from "./user_info";
+import { UserInfo } from "../models/user_info";
 
 @Injectable({
     providedIn: 'root',
-   })
+})
+
 export class AuthService{
 
     private auth_url : string = "/api/auth/";
